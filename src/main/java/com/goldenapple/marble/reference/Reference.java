91605c91e995305a94dd51064cc38c0e503a7e79
goldenapple3 <goldenappleminecraft@gmail.com>
2014-10-31 19:29:45 +0200

    New version of Forge

--- src/main/java/com/goldenapple/marble/reference/Reference.java
+++ src/main/java/com/goldenapple/marble/reference/Reference.java
@@ -3,5 +3,5 @@ package com.goldenapple.marble.reference;
 public class Reference {
     public static final String MOD_ID = "marble";
     public static final String MOD_NAME = "Marble";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
 }
