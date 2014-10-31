d35bbc4463c735c55c7d840bbc54e93fb0e39a62
goldenapple3 <goldenappleminecraft@gmail.com>
2014-10-31 19:35:41 +0200

    alright those commits are getting ridiculous

--- src/main/java/com/goldenapple/marble/init/ModBlocks.java
+++ src/main/java/com/goldenapple/marble/init/ModBlocks.java
@@ -1,6 +1,6 @@
 package com.goldenapple.marble.init;
 
-import com.goldenapple.marble.block.Brick;
+import com.goldenapple.marble.block.BlockBrick;
 import com.goldenapple.marble.block.TallTorch;
 import com.goldenapple.marble.reference.BlockNames;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -8,7 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class ModBlocks {
-    public static final Block brickIron = new Brick(Material.rock, BlockNames.ironBrick, Block.soundTypeStone);
+    public static final Block brickIron = new BlockBrick(Material.rock, BlockNames.ironBrick, Block.soundTypeStone);
     public static final Block tallTorch = new TallTorch();
     public static void init(){
         GameRegistry.registerBlock(brickIron, BlockNames.ironBrick);
