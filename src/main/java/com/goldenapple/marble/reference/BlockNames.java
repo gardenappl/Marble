cba365f98a4a4a87a18030c14e96b13a545750d6
goldenapple3 <goldenappleminecraft@gmail.com>
2014-11-06 18:25:47 +0200

    Renamed BlockNames to Names

--- src/main/java/com/goldenapple/marble/reference/BlockNames.java
+++
@@ -1,6 +0,0 @@
-package com.goldenapple.marble.reference;
-
-public class BlockNames {
-    public static final String ironBrick = "iron_brick";
-    public static final String tallTorch = "tall_torch";
-}
---
+++ src/main/java/com/goldenapple/marble/reference/Names.java
@@ -0,0 +1,8 @@
+package com.goldenapple.marble.reference;
+
+public class Names {
+    public static final String tallTorchTop = "tall_torch_top";
+    public static final String tallTorchBottom = "tall_torch_bottom";
+    public static final String tallTorch = "tall_torch";
+    public static final String glassPaneMulticolor = "glass_pane_multicolor";
+}
